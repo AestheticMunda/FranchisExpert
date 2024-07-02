acc26ceba4e39a88e55eed9312136207e8a6c4d8
Aryaman Agrawal <160284478+AestheticMunda@users.noreply.github.com>
2024-07-02 16:09:19 +0530

    Demo Frame Done

---
+++ constants.ts
@@ -0,0 +1,71 @@
+export const priceRange = [
+  {
+    range: "0 - 50000",
+  },
+  {
+    range: "50000 - 1 Lakh",
+  },
+  {
+    range: "1 Lakh - 2 Lakhs",
+  },
+  {
+    range: "2 Lakhs - 5 Lakhs",
+  },
+  {
+    range: "5 Lakhs - 10 Lakhs",
+  },
+  {
+    range: "10 Lakhs - 15 Lakhs",
+  },
+  {
+    range: "15 Lakhs - 20 Lakhs",
+  },
+  {
+    range: "20 Lakhs - 30 Lakhs",
+  },
+  {
+    range: "30 Lakhs - 40 Lakhs",
+  },
+  {
+    range: "40 Lakhs - 50 Lakhs",
+  },
+  {
+    range: "50 Lakhs - 75 Lakhs",
+  },
+  {
+    range: "75 Lakhs - 1 Crore",
+  },
+];
+
+export const businessCategories = [
+  {
+    name: "Cleaning",
+  },
+  {
+    name: "Day care and Nursery",
+  },
+  {
+    name: "Fitness",
+  },
+  {
+    name: "Food",
+  },
+  {
+    name: "Fashion",
+  },
+  {
+    name: "Health and Beauty",
+  },
+  {
+    name: "Home services",
+  },
+  {
+    name: "Retail",
+  },
+  {
+    name: "Sports",
+  },
+  {
+    name: "Event planning",
+  },
+];
